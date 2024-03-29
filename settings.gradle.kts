7f1311c04adc30299386817206c45f5a9dbf1ab9
omar <108617231+Omar01-m@users.noreply.github.com>
2024-03-29 17:05:49 +0100

    intro activity and login by omar 29/03/2024

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "project 1 v1"
+include(":app")
+ 
