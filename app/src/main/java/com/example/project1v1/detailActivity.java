c1dd0016eb6692e3da00211bd16ce37d1fa4678d
omar <108617231+Omar01-m@users.noreply.github.com>
2024-04-13 13:08:36 +0100

    omar commit N°2 13/04/2024

---
+++ app/src/main/java/com/example/project1v1/detailActivity.java
@@ -0,0 +1,13 @@
+package com.example.project1v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
